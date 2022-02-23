5c4cb3309aeac861fb5b40aa7fdf04b7e0491ab6
eureka <eurekame2000@163.com>
2022-02-23 18:04:21 +0800

    show real emplist

---
+++ src/main/java/com/example/service/EmpServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.service;
+
+import com.example.dao.EmpDAO;
+import com.example.entity.Emp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class EmpServiceImpl implements EmpService{
+
+    @Autowired
+    private EmpDAO empDAO;
+
+    @Override
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public List<Emp> findAll() {
+        return empDAO.findAll();
+    }
+}
